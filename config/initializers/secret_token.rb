d6e2595375ec9af737a334d6995d37599cfa17e5
Julian Arnold <julian.arnold123@gmail.com>
2012-11-11 11:54:35 +0000

    My first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CakeMyDaySite::Application.config.secret_token = '9e33a433e7a60cf9a30f3cc6403f9d1d7bf8d1474e403ab97256e44f46587760f0b9ab9ffd5ca77afad584f7767b0c835e6949aa5c112a0ac9aeb3db3f2700d9'
