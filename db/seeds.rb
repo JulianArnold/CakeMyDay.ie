6150626796deb2df275447074c189a901385c678
danlaffan <dan.laffan@mac.com>
2013-01-19 22:53:20 +0000

    Bringing everything up to date on Github.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-if !Rails.env.development?
+if Rails.env.development?
   
   
   UserGroup.destroy_all
