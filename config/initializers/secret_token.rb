e912f722e67881a6774f3cb418f281f30ed82141
JulianArnold <julian_arnold@hotmail.com>
2014-02-18 15:18:52 +0000

    changed secret token file

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,8 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-CakeMyDaySite::Application.config.secret_token = '9e33a433e7a60cf9a30f3cc6403f9d1d7bf8d1474e403ab97256e44f46587760f0b9ab9ffd5ca77afad584f7767b0c835e6949aa5c112a0ac9aeb3db3f2700d9'
+CakeMyDaySite::Application.config.secret_token = if Rails.env.development? or Rails.env.test?
+                                                   ('x' * 30) # meets minimum requirement of 30 chars long
+                                                 else
+                                                   ENV['SECRET_TOKEN']
+                                                 end
